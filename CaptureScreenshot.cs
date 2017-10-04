54ef21224589360304cecae57905559dc904e8f6
Abdul Wahab <31807031+mirzaabdulwahab1612@users.noreply.github.com>
2017-10-04 21:53:25 +0500

    High quality screen shots in Unity.
    Using this script you can take high quality screen shots in unity during game play. In the script first you have to select the key for which the screen would be captured, and you will have to specify the location of the pictures. The script allows you to take snap shots during game play without pausing it or anything like that. Just attach it to your main canvas and specify the key and address and enjoy :)
    PS: Do make sure to remove the script after taking the screen shots and before creating the build.

---
+++ CaptureScreenshot.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CaptureScreenshot : MonoBehaviour {
+
+	static int number = 1;
+
+    [Range(0, 5)]
+    public float timeScaler = 1f;
+    [Space(2)]
+    public bool enableCanvas = true;
+    [Space(5)]
+    public GameObject mainCanvas;
+
+    void Update ()
+	{
+        Time.timeScale = timeScaler;
+
+		if(Input.GetKeyDown(KeyCode.P))
+		{
+			Application.CaptureScreenshot ("F:\\"+number.ToString() + ".png",3); 
+			number += 1;
+		}
+
+        if (enableCanvas) {
+            mainCanvas.SetActive (true);
+        }
+        else
+        {
+            mainCanvas.SetActive (false);
+        }
+    }
+}
